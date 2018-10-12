3559638c811e2d04bb65b8a9a144f1570a872340
nicktorn89 <nicktorn89@gmail.com>
2018-10-12 17:24:37 +0300

    Add slider and favicon

--- src/index.js
+++ src/index.js
@@ -1,27 +1,55 @@
 let leftButton = document.querySelector(".left-button");
 let rightButton = document.querySelector(".right-button");
+let regButtons = document.querySelectorAll(".reg-buttons");
 
-let sliderImage = document.querySelectorAll(".container > img");
+function Slider() {
+    this.currentSlide = 0;
 
+    this.slider = document.querySelector(".slide-image");
 
-var currentSlide = 0;
-var slideInterval = setInterval(nextSlide, 5000);
+    this.setImage = function() { // Setting attr of src
+        let imageSource = "./images/slide-" + this.currentSlide + ".jpg";
+        this.slider.src = imageSource;
+    }
 
-function stopInterval() {
-    clearInterval(slideInterval);
+    this.stopInterval = function() {
+        clearInterval(this.slideInterval);
+    }
+
+    this.goToSlide = function(n) {
+        this.currentSlide = (n + 15) % 15; // 15 is number of photos
+    }
+
+    this.previousSlide = function() {
+        this.goToSlide(this.currentSlide - 1);
+    }
+
+    this.nextSlide = function() {
+        this.goToSlide(this.currentSlide + 1);
+    }
 }
 
-function nextSlide() {
-    currentSlide += 1;
-    stopInterval();
-    console.log(currentSlide);
+let sliderFunc = new Slider();
+
+sliderFunc.setImage(); // Slider init on first picture with index 0
+
+let slideInterval = setInterval(function() {
+    sliderFunc.nextSlide();
+    sliderFunc.setImage();
+}, 5000)
+
+leftButton.onclick = function() {
+    sliderFunc.previousSlide();
+
+    sliderFunc.stopInterval(); // Stop auto slide
+
+    sliderFunc.setImage();
 }
 
-function previousSlide() {
-    currentSlide -= 1;
-    stopInterval();
-    console.log(currentSlide);
-}
+rightButton.onclick = function() {
+    sliderFunc.nextSlide();
 
-leftButton.addEventListener("click", previousSlide());
-rightButton.addEventListener("click", nextSlide());
+    sliderFunc.stopInterval(); // Stop auto slide
+
+    sliderFunc.setImage();
+}
